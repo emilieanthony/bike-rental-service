dc4c574da31918128f4b715fd82efc762890099e
Emilie Anthony <emilieanthony@live.se>
2022-11-04 07:00:19 +0100

    implement get rentals

--- src/handlers/rentalHandler.go
+++ src/handlers/rentalHandler.go
@@ -12,8 +12,8 @@ type rentalBody struct{
 
 type rental struct {
 	ID string
-	userEmail string
-	bikeID string
+	UserEmail string
+	BikeID string
 }
 
 var rentals []rental
@@ -35,17 +35,34 @@ func RentalHandler(w http.ResponseWriter, req *http.Request){
 		w.WriteHeader(http.StatusCreated)
 		return
 	}
-
+	if req.Method == http.MethodGet{
+		// TODO: Get by id - refactor to gin
+		j, err := json.Marshal(rentals)
+		if err != nil{
+			http.Error (w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write(j)
+		return
+	}
 	w.WriteHeader(http.StatusNotImplemented)
 	return
-
 }
 
 func createRental(userEmail string, bikeID string) error {
 	rentals = append(rentals, rental{
 		ID: "",
-		userEmail: userEmail,
-		bikeID: bikeID,
+		UserEmail: userEmail,
+		BikeID: bikeID,
 	})
+	return nil
+}
+
+func getRentalById (bikeID string) *rental {
+	for _, s := range rentals {
+		if s.BikeID == bikeID{
+			return &s
+		}
+	} 
 	return nil
 }
